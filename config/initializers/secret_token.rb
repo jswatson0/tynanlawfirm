d92cfd8f51056ff0fd5829fa61ceca1abaaabb51
Jason Watson <jswatson0@gmail.com>
2013-08-20 10:53:08 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tynanlawfirm::Application.config.secret_token = '2ccd152d15c2fca579260e8b11b7ae6a9d8a8bbb00cae25fe717f7992d0d4dc79ab7d578a05710faaee51cf09b36354441b324d747b7f1f571f7a8ceac1a987c'
